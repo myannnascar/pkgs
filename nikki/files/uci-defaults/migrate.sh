1eaedfbf668d5647bbf2eb98ef19fda90e4497bd
root <root@vmi1692055.contaboserver.net>
2025-02-21 16:29:38 +0800

    :arrows_counterclockwise: Fri 21 Feb 2025 04:29:38 PM +08

---
+++ nikki/files/uci-defaults/migrate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+. "$IPKG_INSTROOT/etc/nikki/scripts/include.sh"
+
+# since v1.18.0
+
+mixin_rule=$(uci -q get nikki.mixin.rule); [ -z "$mixin_rule" ] && uci set nikki.mixin.rule=0
+
+mixin_rule_provider=$(uci -q get nikki.mixin.rule_provider); [ -z "$mixin_rule_provider" ] && uci set nikki.mixin.rule_provider=0
+
+# commit
+uci commit nikki
+
+# exit with 0
+exit 0
